bdbba5c1894630b55517a7b23785d57005ca96c7
roma_nbz <roma13118@gmail.com>
2024-05-15 15:22:47 +0400

    api truby-b-u

---
+++ types/trubybu/index.ts
@@ -0,0 +1,20 @@
+import type { BalksItems } from '../balks'
+import type { BusBanner } from '../bus'
+import type { Banner, CodeLinkName, LinkText, Seo, TitleUrl } from '../root'
+
+export interface TrubybuDto {
+	data: TrubybuData
+	error: string[]
+	success: string
+}
+
+export interface TrubybuData {
+	btnLink: LinkText
+	file: string
+	info: Banner
+	items: BalksItems[]
+	menu: TitleUrl[]
+	seo: Seo
+	sibar_filter: CodeLinkName[]
+	sidebar_banner: BusBanner[]
+}
