63cc09f07e033b18fdf13c700bbeba0e4ee8c2c6
roma_nbz <roma13118@gmail.com>
2024-05-15 21:22:59 +0400

    api truby-b-u id

--- pages/truby-b-u/[id].vue
+++ pages/truby-b-u/[id].vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-  const category = ref({
-  template: '',
-  priceList: 'https://ooosgr.ru/files/pipe/category/5a97a18be80e79e940079c3f3b8fc169a29c9e7f9.pdf',
-  name: 'КУПИТЬ ТРУБЫ 108 БУ С ДЕМОНТАЖА',
-  previewTextHtml: '<p>Стальные трубы бу диаметром 108 мм, ранее применялись в различных инженерных системах, таких как водоснабжение, отопление, газоснабжение или промышленные технологические процессы. Такая труба бу 108 мм обычно имеет защитное покрытие от коррозии, а прочность и долговечность делает ее популярным выбором для широкого спектра последующих применений. Приобретая или используя стальные трубы б у 108 мм, необходимо убедиться, что они соответствуют всем актуальным стандартам и нормам безопасности.</p>',
+import type { TrubybuDto } from '~/types/trubybu'
+
+const route = useRoute()
+const id = computed(() => route.path.split('/').pop())
+
+const { data: trubybuid } = await useContentFetch<TrubybuDto>(`truby-b-u/${id.value}`, {
+  method: 'GET'
+})
+
+const template = ref('')
+
+useServerSeoMeta({
+  ogTitle: () => trubybuid.value!.data.seo.title,
+  title: () => trubybuid.value!.data.seo.title,
+  description: () => trubybuid.value!.data.seo.description,
+  ogDescription: () => trubybuid.value!.data.seo.description,
+  keywords: () => trubybuid.value!.data.seo.keywords
+})
+
+const data = ref({
   slider: [
     {
       big: 'https://ooosgr.ru/cache/pipe_category_fancybox/files/pipe/pipe/photos/47/88b08f5e2f500a2c1918bb55109c5e56ae13961e.jpeg',
@@ -25,53 +40,11 @@
     }
   ],
   showMenu: true,
-  textHtml: '<p><strong>Трубы бу диаметром 114 мм</strong> являются популярным видом трубопроката, который широко используется в различных отраслях промышленности и строительства. Такие трубы характеризуются относительно небольшим размером и высокой прочностью, что делает их универсальным и удобным в использовании материалом.</p><p>Одним из основных преимуществ труб бу диаметром 114 мм является их доступная стоимость. При этом качество изделий остается на высоком уровне, что позволяет существенно снизить затраты на строительство или ремонт объектов. Кроме того, такие трубы характеризуются высокой степенью износостойкости и долговечности, что гарантирует долговечность конструкций.</p><p>Трубы бу диаметром 114 мм широко используются в различных областях, таких как строительство трубопроводов, газопроводов, водопроводов, систем отопления и вентиляции, а также для изготовления металлических конструкций. Они также находят применение в производстве мебели и других изделий из металла.</p><p>Купить трубы бу диаметром 114 мм легко и выгодно в фирме "СтройГеоРесурс". Мы предлагаем широкий выбор трубопроката в наличии и под заказ, в том числе трубы бу высокого качества. У нас вы найдете как тонкостенные, так и <a href="https://ooosgr.ru/truby-b-u">толстостенные трубы бу</a>, а также изделия различных диаметров и размеров. Мы гарантируем быструю доставку заказа и индивидуальный подход к каждому клиенту.</p>',
   itemsHead: [
     'Диаметр',
     'Описание',
     'Цена',
     'Фото'
-  ],
-  items: [
-    {
-      name: '108×6',
-      htmlName: '108<span style="font-size:11px">х</span>6',
-      mobilePrefixName: 'ø',
-      text: 'Труба бу без изоляции, вся длинная, ровная, внутри чистая',
-      priceFormated: '44 500 руб.',
-      img: {
-        big: 'https://ooosgr.ru/cache/pipe_category_fancybox/files/pipe/pipe/photos/47/88b08f5e2f500a2c1918bb55109c5e56ae13961e.jpeg',
-        small: 'https://ooosgr.ru/cache/pipe_category_small/files/pipe/pipe/photos/47/88b08f5e2f500a2c1918bb55109c5e56ae13961e.jpeg',
-        title: '108×6 мм',
-        rel: 'pipe47',
-      }
-    },
-    {
-      name: '108×5',
-      htmlName: '108<span style="font-size:11px">х</span>5',
-      mobilePrefixName: 'ø',
-      text: 'Труба бу 108 с нерабочей ветки, полностью вся чистая, с правильной геометрией',
-      priceFormated: '46 000 руб.',
-      img: {
-        big: 'https://ooosgr.ru/cache/pipe_category_fancybox/files/pipe/pipe/photos/47/88b08f5e2f500a2c1918bb55109c5e56ae13961e.jpeg',
-        small: 'https://ooosgr.ru/cache/pipe_category_small/files/pipe/pipe/photos/47/88b08f5e2f500a2c1918bb55109c5e56ae13961e.jpeg',
-        title: '108×5 мм',
-        rel: 'pipe47',
-      }
-    },
-    {
-      name: '108×7',
-      htmlName: '108<span style="font-size:11px">х</span>7',
-      mobilePrefixName: 'ø',
-      text: 'Бу труба 108 отличного качества, внутри чистая, снаружи частичный битум',
-      priceFormated: '45 000 руб.',
-      img: {
-        big: 'https://ooosgr.ru/cache/pipe_category_fancybox/files/pipe/pipe/photos/47/88b08f5e2f500a2c1918bb55109c5e56ae13961e.jpeg',
-        small: 'https://ooosgr.ru/cache/pipe_category_small/files/pipe/pipe/photos/47/88b08f5e2f500a2c1918bb55109c5e56ae13961e.jpeg',
-        title: '108×7 мм',
-        rel: 'pipe47',
-      }
-    },
   ]
 })
 </script>
@@ -81,8 +54,17 @@
   <div class="wrap content page pipe">
     <div class="wrap_cont">
       <div class="two columns columns--adaptive">
-        <Sidebar />
-        <Category :category="category" />
+        <Sidebar
+          v-if="trubybuid"
+          :data="trubybuid?.data"
+        />
+        <Category
+          v-if="trubybuid"
+          :content="trubybuid?.data"
+          :category="trubybuid?.data.info"
+          :template
+          :data
+        />
       </div>
     </div>
   </div>
@@ -90,6 +72,4 @@
   <div class="push" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
